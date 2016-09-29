ecee5045a306c3c2ce0e77c3ee3d37459b3bc8a8
jack <jackofspades707@hotmail.com>
2016-09-29 19:18:36 +0000

    started

---
+++ instantrom.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+class InstantRom:
+    def __init__(self, proxy=None):
+        self._proxy = self.setup_proxy(proxy)
+        self._headers = {}
+        self._session = requests.Session()
+
+    def setup_proxy(self, proxy):
+        if proxy is None:
+            return None
+        return {'http': 'http://' + proxy, 'https': 'https://' + proxy}
+
+
+    def search(self, query):
+
+
