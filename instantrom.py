35250a7eae55da578a22fa1e6dbce539f04a859d
jack <jackofspades707@hotmail.com>
2016-09-30 02:20:59 +0000

    emuparadise had a captcha, changed host website

--- instantrom.py
+++ instantrom.py
@@ -18,17 +18,25 @@ class InstantRom:
             return None
         return {'http': 'http://' + proxy, 'https': 'https://' + proxy}
 
-
     def search(self, query=input('Enter your query\n> ')):
-        url = 'http://www.emuparadise.me/roms/search.php?query={}&section=roms'.format(query)
+        url = 'http://www.doperoms.com/search.php?s={}&method=ROM+SEARCH'.format(query)
         r = self._session.get(url)
-        roms = BeautifulSoup(r.content, 'html.parser').findAll(class_='roms')
+        roms = BeautifulSoup(r.content, 'html.parser').findAll(id='listing')
         for i, rom in enumerate(roms):
-            print('[{}] {}'.format(i, rom.get_text()))  #  TODO: make this prettier
+            if rom.get_text() == '':
+                del(roms[i])
+        for i, rom in enumerate(roms):
+            print('[{}] {} | {}'.format(i, rom.get_text(), rom.find_next().find_next().get_text()))  # TODO: make this prettier
             if i % 10 == 0 and i != 0:
                 prompt = input('> ')
+                if 'q' in prompt:
+                    print('quiting')
+                    raise SystemExit
                 if prompt.isnumeric():
-                    return 'http://www.emuparadise.me' + roms[int(prompt)].find('a')['href']
+                    return 'http://www.doperoms.com/' + roms[int(prompt)]['href']
+
+    def download(self, url):
+        r = self._session.get(url)
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 # Rom Downloader
+
+# TODO
+* Download function
+* Add install instructions
+* Add man page instructions
+* Make CLI threaded functionality
+
