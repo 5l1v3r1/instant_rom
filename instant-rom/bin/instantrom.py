41eee76734b3d1f8fe5140efad2dc67fb6a68578
jackofspades707 <jackofspades707@hotmail.com>
2016-10-06 00:15:21 -0700

    simplified a bit

--- instant-rom/bin/instantrom.py
+++ instant-rom/bin/instantrom.py
@@ -5,13 +5,18 @@ import zipfile
 
 import requests
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-m', '--max_results', help='max results to output', type=int, default=10)
+args = parser.parse_args()
 
 
 class InstantRom:
     def __init__(self, proxy=None):
         self._session = requests.Session()
         self._session.proxies = self.setup_proxy(proxy)
-        self._session.headers.update({'User-Agent': None})  #TODO: Add valud UA
+        self._session.headers.update({'User-Agent': None})  #TODO: Add valid UA
 
     def setup_proxy(self, proxy):
         '''
@@ -32,34 +37,29 @@ class InstantRom:
         :param: query = search query to find roms
         :returns: list of selected roms (to be downloaded)
         '''
-        results = []
         url = 'http://www.doperoms.com/search.php?s={}&method=ROM+SEARCH'.format(query)
         r = self._session.get(url)
         roms = BeautifulSoup(r.content, 'html.parser').findAll(id='listing')
         for i, rom in enumerate(roms):
             if rom.get_text() == '':
                 del(roms[i])
-        print('y - yes (done) | n - next | q - quit')
         for i, rom in enumerate(roms):
             rom_name = rom.get_text()
             system = rom.find_next().find_next().get_text()
+            if i > args.max_results:
+                break
             print('[{}] {} | {}'.format(i, system, rom_name))
-            if i % 10 == 0 and i != 0:
-                prompt = input('> ')
-                if 'q' in prompt:
-                    print('quiting')
-                    raise SystemExit
-                if 'y' in prompt:
-                    break
-                if prompt.isnumeric():
-                    url = 'http://www.doperoms.com/' + roms[int(prompt)]['href']
-                    r = self._session.get(url)
-                    url = 'http://www.doperoms.com' + BeautifulSoup(r.content, 'html.parser').center.find('a')['href']
-                    r = self._session.get(url)
-                    url = BeautifulSoup(r.content, 'html.parser').find(style='padding:15px; width: 400px; border:1px dashed #000000;').findAll('a')[1]['href']
-                    result_name = roms[int(prompt)].get_text()
-                    results.append(('http://www.doperoms.com' + url, result_name))
-        return results
+        try:
+            prompt = int(input('> '))
+        except ValueError:
+            raise SystemExit
+        url = 'http://www.doperoms.com/' + roms[prompt]['href']
+        r = self._session.get(url)
+        url = 'http://www.doperoms.com' + BeautifulSoup(r.content, 'html.parser').center.find('a')['href']
+        r = self._session.get(url)
+        url = BeautifulSoup(r.content, 'html.parser').find(style='padding:15px; width: 400px; border:1px dashed #000000;').findAll('a')[1]['href']
+        result_name = roms[prompt].get_text()
+        return ['http://www.doperoms.com' + url, result_name]
 
     def download(self, url, filename):
         '''
@@ -79,13 +79,9 @@ class InstantRom:
         if delete is True:
             os.remove(filename)
 
-    def queue(self):
-        '''
-        homebrew manager/queue
-        '''
+    def main(self):
         q = self.search()
-        for i in q:
-            self.download(i[0], i[1])
+        self.download(q[0], q[1])
 
 if __name__ == '__main__':
-    InstantRom().queue()
+    InstantRom().main()
